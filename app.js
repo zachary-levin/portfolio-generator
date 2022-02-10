dd1c4e56e855842b5d97c19ee5ddec22a24efe16
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-10 00:40:36 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+console.log(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
-/* const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-
-*/
