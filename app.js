28a632ac5ab2a57ec0931c48969483dc0ed66628
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-10 22:27:49 -0600

    created the .gitignore file

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+/*
 const fs = require('fs');
 const generatePage = require('./src/page-template');
-
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
 
 const pageHTML = generatePage(name, github);
 
@@ -16,3 +21,4 @@ fs.writeFile('./index.html', pageHTML, err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
