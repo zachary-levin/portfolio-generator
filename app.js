32544347d3805c84ac654f93b67898d614a51ccd
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-12 14:28:07 -0600

    Generate entire website

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -129,12 +129,37 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-     fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-       console.log('Page created! Check out index.html in this directory to see it!');
-     });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+    //  fs.writeFile('./dist/index.html', pageHTML, err => {
+    //    if (err) {
+    //      console.log(err);
+    //      return;
+    //    }
+    //    console.log('Page created! Check out index.html in this directory to see it!');
+
+    //    fs.copyFile('./src/style.css', './dist/style.css', err => {
+    //      if (err) {
+    //        console.log(err);
+    //        return;
+    //      }
+    //      console.log('Style sheet copied successfully!');
+    //    });
+    //  });
 
 /*
 const fs = require('fs');
