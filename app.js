0bb4ab3286f4124a09637a30003080f12f2719b7
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-12 14:37:16 -0600

    cleaned up commented code in app.js file

--- app.js
+++ app.js
@@ -144,32 +144,3 @@ promptUser()
   .catch(err => {
     console.log(err);
   });
-
-    //  fs.writeFile('./dist/index.html', pageHTML, err => {
-    //    if (err) {
-    //      console.log(err);
-    //      return;
-    //    }
-    //    console.log('Page created! Check out index.html in this directory to see it!');
-
-    //    fs.copyFile('./src/style.css', './dist/style.css', err => {
-    //      if (err) {
-    //        console.log(err);
-    //        return;
-    //      }
-    //      console.log('Style sheet copied successfully!');
-    //    });
-    //  });
-
-/*
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-*/
